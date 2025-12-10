e97d0ae9325295a458920b5a06d5b0e985e9152d
lodhi.ji.16 <lodhi.ji.16@gmail.com>
2025-12-10 17:49:39 +0000

    Changes before Firebase Studio auto-run

--- lib/features/profile/presentation/bloc/events/profile_event.dart
+++ lib/features/profile/presentation/bloc/events/profile_event.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../../domain/entities/user_profile.dart';
+import '../../../domain/entities/user_profile.dart';
 
 @immutable
 abstract class ProfileEvent {}
